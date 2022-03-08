3d7d35bf2815c6c046b78c80152476eaa835e37a
Félix Viart <viart.felix2017@gmail.com>
2022-03-08 14:30:01 +0100

    Uncommit bruv

--- code/server/data/initdb.js
+++ code/server/data/initdb.js
@@ -14,13 +14,13 @@ let db = new sqlite3.Database('./data/ecoalDB', sqlite3.OPEN_READWRITE | sqlite3
 
 let query = [
 "PRAGMA foreign_keys = ON",
-"DROP TABLE IF EXISTS articles",
+"DROP TABLE IF EXISTS article",
 "DROP TABLE IF EXISTS tag",
 "DROP TABLE IF EXISTS article_tag",
-"CREATE TABLE articles (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, thumbnailURL TEXT, mediaType TEXT, mediaURL TEXT, leadStory INTEGER, publish DATETIME)",
+"CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, thumbnailURL TEXT, mediaType TEXT, mediaURL TEXT, leadStory INTEGER, publish DATETIME)",
 "CREATE TABLE tag (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)",
-"CREATE TABLE article_tag (id INTEGER PRIMARY KEY AUTOINCREMENT, idArticle INTEGER REFERENCES articles(id), idTag INTEGER REFERENCES tag(id))",
-"INSERT INTO articles (title, content, thumbnailURL, mediaType, mediaURL, publish) values ('Welcome to ecoal22', '<h1>Hello from ecoal</h1><p>Nice to see you in <strong>Lens</strong>. Enjoy !</p>', 'ecoal.jpg', 'image', 'ecoal.jpg', '2022-03-08 13:14:09')",
+"CREATE TABLE article_tag (id INTEGER PRIMARY KEY AUTOINCREMENT, idArticle INTEGER REFERENCES article(id), idTag INTEGER REFERENCES tag(id))",
+"INSERT INTO article (title, content, thumbnailURL, mediaType, mediaURL, publish) values ('Welcome to ecoal22', '<h1>Hello from ecoal</h1><p>Nice to see you in <strong>Lens</strong>. Enjoy !</p>', 'ecoal.jpg', 'image', 'ecoal.jpg', '2022-03-08 13:14:09')",
 "INSERT INTO tag (name) values ('ecoal22')",
 "INSERT INTO tag (name) values ('reactJS')",
 "INSERT INTO article_tag (idArticle, idTag) values (1,1)",
