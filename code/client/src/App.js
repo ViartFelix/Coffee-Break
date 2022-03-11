62cfbc578e63dca4acedee1632c92f408dbb5bcf
Moucheron <enzomoucheron@hotmail.fr>
2022-03-11 18:43:25 +0100

    header

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -7,8 +7,16 @@ function App() {
   return (
     <>
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/articles">News</Link>
+      <div className="Title">
+        <svg className="burger" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+        <h1>COFFEE</h1>
+        <img src='/logo.png' alt='Logo' className="logo"></img>
+        <h1>BREAK</h1>
+        <img src='/avatar.png' alt='avatar' className="avatar"></img>
+        {/* <Link to="/">Home</Link>
+        <Link to="/articles">News</Link> */}
+      </div>
     </nav>
 
       <Routes>
